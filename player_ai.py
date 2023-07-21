130bbe0d432e49970a3b53d2f86887ae97c0a1c2
ValMobBIllich <120391217+ValMobBIllich@users.noreply.github.com>
2023-07-21 14:10:27 +0200

    added our bot

--- player_ai.py
+++ player_ai.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 # This is your team name
-CREATOR = "TeamName"
+CREATOR = "The Wise Riders"
 
 
 # This is the AI bot that will be instantiated for the competition
@@ -46,66 +46,14 @@ class PlayerAi:
             A 2D numpy array containing the game map.
             1 means land, 0 means water, -1 means no info.
         """
-
-        # Get information about my team
+        if t < 180: # 3 minutes
+            self.strategy_early(info=info)
+        elif t < 360: # 6 minutes
+            self.strategy_midgame(info=info)
+        else:
+            self.strategy_lategame(info=info)
+            
         myinfo = info[self.team]
-
-        # Controlling my bases =================================================
-
-        # Description of information available on bases:
-        #
-        # This is read-only information that all the bases (enemy and your own) have.
-        # We define base = info[team_name_1]["bases"][0]. Then:
-        #
-        # base.x (float): the x position of the base
-        # base.y (float): the y position of the base
-        # base.position (np.ndarray): the (x, y) position as a numpy array
-        # base.team (str): the name of the team the base belongs to, e.g. ‘John’
-        # base.number (int): the player number
-        # base.mines (int): the number of mines inside the base
-        # base.crystal (int): the amount of crystal the base has in stock
-        #     (crystal is per base, not shared globally)
-        # base.uid (str): unique id for the base
-        #
-        # Description of base methods:
-        #
-        # If the base is your own, the object will also have the following methods:
-        #
-        # base.cost("mine"): get the cost of an object.
-        #     Possible types are: "mine", "tank", "ship", "jet"
-        # base.build_mine(): build a mine
-        # base.build_tank(): build a tank
-        # base.build_ship(): build a ship
-        # base.build_jet(): build a jet
-
-        # Iterate through all my bases (vehicles belong to bases)
-        for base in myinfo["bases"]:
-            # If this is a new base, initialize the tank & ship counters
-            if base.uid not in self.ntanks:
-                self.ntanks[base.uid] = 0
-            if base.uid not in self.nships:
-                self.nships[base.uid] = 0
-            # Firstly, each base should build a mine if it has less than 3 mines
-            if base.mines < 3:
-                if base.crystal > base.cost("mine"):
-                    base.build_mine()
-            # Secondly, each base should build a tank if it has less than 5 tanks
-            elif base.crystal > base.cost("tank") and self.ntanks[base.uid] < 5:
-                # build_tank() returns the uid of the tank that was built
-                tank_uid = base.build_tank(heading=360 * np.random.random())
-                # Add 1 to the tank counter for this base
-                self.ntanks[base.uid] += 1
-            # Thirdly, each base should build a ship if it has less than 3 ships
-            elif base.crystal > base.cost("ship") and self.nships[base.uid] < 3:
-                # build_ship() returns the uid of the ship that was built
-                ship_uid = base.build_ship(heading=360 * np.random.random())
-                # Add 1 to the ship counter for this base
-                self.nships[base.uid] += 1
-            # If everything else is satisfied, build a jet
-            elif base.crystal > base.cost("jet"):
-                # build_jet() returns the uid of the jet that was built
-                jet_uid = base.build_jet(heading=360 * np.random.random())
-
         # Try to find an enemy target
         target = None
         # If there are multiple teams in the info, find the first team that is not mine
@@ -120,60 +68,26 @@ class PlayerAi:
 
         # Controlling my vehicles ==============================================
 
-        # Description of information available on vehicles
-        # (same info for tanks, ships, and jets):
-        #
-        # This is read-only information that all the vehicles (enemy and your own) have.
-        # We define tank = info[team_name_1]["tanks"][0]. Then:
-        #
-        # tank.x (float): the x position of the tank
-        # tank.y (float): the y position of the tank
-        # tank.team (str): the name of the team the tank belongs to, e.g. ‘John’
-        # tank.number (int): the player number
-        # tank.speed (int): vehicle speed
-        # tank.health (int): current health
-        # tank.attack (int): vehicle attack force (how much damage it deals to enemy
-        #     vehicles and bases)
-        # tank.stopped (bool): True if the vehicle has been told to stop
-        # tank.heading (float): the heading angle (in degrees) of the direction in
-        #     which the vehicle will advance (0 = east, 90 = north, 180 = west,
-        #     270 = south)
-        # tank.vector (np.ndarray): the heading of the vehicle as a vector
-        #     (basically equal to (cos(heading), sin(heading))
-        # tank.position (np.ndarray): the (x, y) position as a numpy array
-        # tank.uid (str): unique id for the tank
-        #
-        # Description of vehicle methods:
-        #
-        # If the vehicle is your own, the object will also have the following methods:
-        #
-        # tank.get_position(): returns current np.array([x, y])
-        # tank.get_heading(): returns current heading in degrees
-        # tank.set_heading(angle): set the heading angle (in degrees)
-        # tank.get_vector(): returns np.array([cos(heading), sin(heading)])
-        # tank.set_vector(np.array([vx, vy])): set the heading vector
-        # tank.goto(x, y): go towards the (x, y) position
-        # tank.stop(): halts the vehicle
-        # tank.start(): starts the vehicle if it has stopped
-        # tank.get_distance(x, y): get the distance between the current vehicle
-        #     position and the given point (x, y) on the map
-        # ship.convert_to_base(): convert the ship to a new base (only for ships).
-        #     This only succeeds if there is land close to the ship.
-        #
-        # Note that by default, the goto() and get_distance() methods will use the
-        # shortest path on the map (i.e. they may go through the map boundaries).
-
         # Iterate through all my tanks
         if "tanks" in myinfo:
             for tank in myinfo["tanks"]:
+                tank_target = None
+                bases = [base for name in info for base in info[name].get('bases', []) if name != self.team]
+                nearest_base = None
+                nearest_base = sorted(bases, key=lambda b: (b.x - tank.x) ** 2 + (b.y - tank.y) ** 2)
+                if nearest_base:
+                    tank_target = nearest_base[0]
                 if (tank.uid in self.previous_positions) and (not tank.stopped):
                     # If the tank position is the same as the previous position,
                     # set a random heading
                     if all(tank.position == self.previous_positions[tank.uid]):
                         tank.set_heading(np.random.random() * 360.0)
                     # Else, if there is a target, go to the target
-                    elif target is not None:
-                        tank.goto(*target)
+                    elif tank_target is not None:
+                        try:
+                            tank.goto(*tank_target)
+                        except Exception:
+                            pass
                 # Store the previous position of this tank for the next time step
                 self.previous_positions[tank.uid] = tank.position
 
@@ -192,9 +106,128 @@ class PlayerAi:
                 # Store the previous position of this ship for the next time step
                 self.previous_positions[ship.uid] = ship.position
 
+        target = None
+        # If there are multiple teams in the info, find the first team that is not mine
+        def determine_base_power(base):
+            return base.mines * 10 + base.crystal/10
+
+        def determine_power(team):
+            return 100 * len(team.get('bases', [])) + sum(determine_base_power(base) for base in team.get('bases', []))
+
+        if len(info) > 1:
+            teams = [team for name, team in info.items() if name != self.team]
+            by_power = sorted(teams, key=determine_power)
+            if by_power:
+                strongest_enemy = by_power[-1]
+                if not 'bases' in strongest_enemy:
+                    target = [75,75]
+                else:
+                    strongest_base = sorted(strongest_enemy.get('bases', []), key=determine_base_power)
+                    if not strongest_base:
+                        target = [75,75]
+                    else:
+                        strongest_base = strongest_base[-1]
+                        target = [strongest_base.x, strongest_base.y]
+
         # Iterate through all my jets
         if "jets" in myinfo:
             for jet in myinfo["jets"]:
                 # Jets simply go to the target if there is one, they never get stuck
                 if target is not None:
                     jet.goto(*target)
+
+
+    def strategy_early(self, info):
+        # Get information about my team
+        myinfo = info[self.team]
+        max_ships_per_base = 6
+        max_mines_per_base = 3
+        # Controlling my bases =================================================
+
+        # Iterate through all my bases (vehicles belong to bases)
+        for base in myinfo["bases"]:
+            # If this is a new base, initialize the tank & ship counters
+            if base.uid not in self.ntanks:
+                self.ntanks[base.uid] = 0
+            if base.uid not in self.nships:
+                self.nships[base.uid] = 0
+            # Firstly, each base should build a mine if it has less than 3 mines
+            if base.mines < max_mines_per_base:
+                if base.crystal > base.cost("mine"):
+                    base.build_mine()
+            elif base.crystal > base.cost("tank") and self.ntanks[base.uid] < 2:
+                # build_tank() returns the uid of the tank that was built
+                tank_uid = base.build_tank(heading=360 * np.random.random())
+                # Add 1 to the tank counter for this base
+                self.ntanks[base.uid] += 1
+            # Thirdly, each base should build a ship if it has less than 3 ships
+            elif base.crystal > base.cost("ship") and self.nships[base.uid] < max_ships_per_base:
+                # build_ship() returns the uid of the ship that was built
+                ship_uid = base.build_ship(heading=360 * np.random.random())
+                # Add 1 to the ship counter for this base
+                self.nships[base.uid] += 1
+            # Secondly, each base should build a tank if it has less than 5 tanks
+            elif base.crystal > base.cost("tank") and self.ntanks[base.uid] < 5 and self.nships[base.uid] >= max_ships_per_base:
+                # build_tank() returns the uid of the tank that was built
+                tank_uid = base.build_tank(heading=360 * np.random.random())
+                # Add 1 to the tank counter for this base
+                self.ntanks[base.uid] += 1
+            # If everything else is satisfied, build a jet
+            elif base.crystal > base.cost("jet"):
+                # build_jet() returns the uid of the jet that was built
+                jet_uid = base.build_jet(heading=360 * np.random.random())
+
+    def strategy_midgame(self, info):
+            # Get information about my team
+            myinfo = info[self.team]
+
+            # Controlling my bases =================================================
+
+            # Iterate through all my bases (vehicles belong to bases)
+            for base in myinfo["bases"]:
+                # If this is a new base, initialize the tank & ship counters
+                if base.uid not in self.ntanks:
+                    self.ntanks[base.uid] = 0
+                if base.uid not in self.nships:
+                    self.nships[base.uid] = 0
+                # Firstly, each base should build a mine if it has less than 3 mines
+                if base.mines < 2:
+                    if base.crystal > base.cost("mine"):
+                        base.build_mine()
+                elif base.crystal > base.cost("tank") and self.ntanks[base.uid] < 2:
+                    # build_tank() returns the uid of the tank that was built
+                    tank_uid = base.build_tank(heading=360 * np.random.random())
+                    # Add 1 to the tank counter for this base
+                    self.ntanks[base.uid] += 1
+                # Thirdly, each base should build a ship if it has less than 3 ships
+                elif base.crystal > base.cost("ship") and self.nships[base.uid] < 2:
+                    # build_ship() returns the uid of the ship that was built
+                    ship_uid = base.build_ship(heading=360 * np.random.random())
+                    # Add 1 to the ship counter for this base
+                    self.nships[base.uid] += 1
+                # Secondly, each base should build a tank if it has less than 5 tanks
+                # If everything else is satisfied, build a jet
+                elif base.crystal > base.cost("jet"):
+                    # build_jet() returns the uid of the jet that was built
+                    jet_uid = base.build_jet(heading=360 * np.random.random())
+
+
+
+    def strategy_lategame(self, info):
+            # Get information about my team
+            myinfo = info[self.team]
+
+            # Controlling my bases =================================================
+
+            # Iterate through all my bases (vehicles belong to bases)
+            for base in myinfo["bases"]:
+                # If this is a new base, initialize the tank & ship counters
+                if base.uid not in self.ntanks:
+                    self.ntanks[base.uid] = 0
+                if base.uid not in self.nships:
+                    self.nships[base.uid] = 0
+                # If everything else is satisfied, build a jet
+                if base.crystal > base.cost("jet"):
+                    # build_jet() returns the uid of the jet that was built
+                    jet_uid = base.build_jet(heading=360 * np.random.random())
+
